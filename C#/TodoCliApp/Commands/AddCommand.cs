b72d195af9697752094518305cfe0d7dee5d0e6c
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2026-04-27 16:02:25 -0400

    Inject TodoService and implement add command
    AddCommand now takes a TodoService and defines a proper "add" subcommand with a title argument, --priority/-p option and --due/-d option. The command parses inputs, validates the priority enum and non-empty title, prints the values for debug, and calls service.AddTodoAsync to persist the todo. Program.cs is updated to instantiate JsonTodoRepository and TodoService, construct the AddCommand with the service, register the subcommand on the root command, and invoke the parser. Added necessary using directives for repository/services and command parsing.

--- C#/TodoCliApp/Commands/AddCommand.cs
+++ C#/TodoCliApp/Commands/AddCommand.cs
@@ -2,22 +2,55 @@
 using System.Collections.Generic;
 using System.Text;
 using System.CommandLine;
-
+using TodoCliApp.Services;
+using TodoCliApp.Repository;
+using TodoCliApp.Models;
 
 namespace TodoCliApp.Commands
 {
     internal class AddCommand
     {
         public Command addCommand { get; }
+        private readonly TodoService _service;
 
-            public AddCommand()
-            {
-                // Defining the subcommand to add
-                addCommand = new Command("add", "use to add tasks");
+        public AddCommand(TodoService service)
+        {
+            _service = service;
+
+            // Defining the subcommand to add
+            addCommand = new Command("add", "use to add tasks");
 
                 //Defining the options for subcommand
-                Option<string> PriorityOption = new Option<string>("--priority", "Use this to add urgency of task");
-            }
-        
+                Argument<string> titleArgument = new Argument<string>("title");
+                Option<string> PriorityOption = new Option<string>("--priority", "-p")
+                    {
+                    Description = "Use this to add urgency of task"
+                     };
+                Option<DateTime> dueDateOption = new Option<DateTime>("--due", "-d")
+                {
+                Description = "Use this to add due of task"
+                };
+
+            addCommand.Add(titleArgument);
+            addCommand.Options.Add(PriorityOption);
+            addCommand.Options.Add(dueDateOption);
+
+            addCommand.SetAction(async parseResult =>
+            {
+                var titleValue = parseResult.GetValue(titleArgument);
+                var priorityValue = parseResult.GetValue(PriorityOption);
+                var dateTimeValue = parseResult.GetValue(dueDateOption);
+
+
+                Console.WriteLine($"{titleValue}, {priorityValue}, {dateTimeValue}");
+
+                if (Enum.TryParse(priorityValue, true, out Priority value) && !string.IsNullOrWhiteSpace(titleValue))
+                {
+                    await _service.AddTodoAsync(titleValue, value, dateTimeValue);
+                }                
+            });
+
+        }
+
     }
 }
--- C#/TodoCliApp/Program.cs
+++ C#/TodoCliApp/Program.cs
@@ -1,7 +1,20 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Parsing;
 using TodoCliApp.Commands;
+using TodoCliApp.Repository;
+using TodoCliApp.Services;
 
+//dependencys
+var jsonRepo = new JsonTodoRepository();
+var services = new TodoService(jsonRepo);
+var addCommand = new AddCommand(services);
 
-
+// rootCommand
 var rootCommand = new RootCommand("Sample command-line TO-Do app");
 
+//SubCommands
+
+
+rootCommand.Subcommands.Add(addCommand.addCommand);
+
+return rootCommand.Parse(args).Invoke();
