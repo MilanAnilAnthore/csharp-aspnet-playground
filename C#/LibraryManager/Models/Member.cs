b6f3d6b5446b0f024b7c931193b9b1f397667692
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2026-05-01 01:10:02 -0400

    Add IMember and make Member implement it
    Introduce a new IMember interface under Repository and update Member to implement it. Converted memberID and BorrowedBookIds from fields to auto-properties and added the Repository using in Member.cs to reference the interface. This prepares the model for abstraction and dependency injection.

--- C#/LibraryManager/Models/Member.cs
+++ C#/LibraryManager/Models/Member.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using LibraryManager.Abstractions;
+using LibraryManager.Repository;
 
 namespace LibraryManager.Models
 {
-    public class Member
+    public class Member : IMember
     {
-        public Guid memberID;
+        public Guid memberID { get; set; }
         public string Name { get; set; } = "";
 
-        public List<string> BorrowedBookIds = [];
+        public List<string> BorrowedBookIds { get; set; } = [];
     }
 }
---
+++ C#/LibraryManager/Repository/IMember.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Repository
+{
+    public interface IMember
+    {
+        public Guid memberID { get; set; }
+        public string Name { get; set; }
+
+        public List<string> BorrowedBookIds { get; set; }
+    }
+}
