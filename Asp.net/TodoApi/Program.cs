a7fed7089f501fc8d965cb1aad5a787efb81cc22
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2026-05-09 22:38:19 -0400

    Add Swagger (Swashbuckle) support to Todo API
    Add Swashbuckle.AspNetCore package and configure Swagger generation/UI. Tired today, i guess that it

--- Asp.net/TodoApi/Program.cs
+++ Asp.net/TodoApi/Program.cs
@@ -12,6 +12,15 @@ var builder = WebApplication.CreateBuilder(args);
                  options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
              });
     builder.Services.AddOpenApi();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+        { 
+            Title = "Todo API", 
+            Version = "v1",
+            Description = "An API to manage Todo items"
+        });
+    });
     builder.Services.AddScoped<ITodoRepository, JsonTodoRepository>();
     builder.Services.AddScoped<IServiceRepository, TodoService>();
 }
@@ -28,6 +37,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 app.Run();
 
